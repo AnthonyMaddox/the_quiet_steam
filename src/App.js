76aa0d2216fa23978f3a7f47117e88195fc1bd68
AnthonyMaddox <anthonyjmaddox@gmail.com>
2023-02-22 19:02:07 -0500

    added images and parallax effect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useRouteMatch,
+  useParams,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {/* <div className="main">The Quiet Steam</div> */}
+      <section className="image-1"></section>
+      <section className="image-2"></section>
+      <section className="image-3"></section>
     </div>
   );
 }
